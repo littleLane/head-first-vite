50e32dfecd1692f8823944615db50a55326113f3
littleLane <lzz857183384@163.com>
2022-04-27 22:21:09 +0800

    feat: 通过 vite.config 中的 server.port 指定启动的端口

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+/*
+ * @Author: qianzhi
+ * @Date: 2022-04-22 23:35:58
+ * @LastEditors: qianzhi
+ * @LastEditTime: 2022-04-27 22:20:13
+ * @FilePath: /head-first-vite/vite.config.ts
+ */
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
-})
+  plugins: [react()],
+  server: {
+    port: 8888,
+  },
+});
