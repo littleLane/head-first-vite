03d4d42efdcd5cdd69363e595de25828ef367cf9
littleLane <lzz857183384@163.com>
2022-04-27 22:54:52 +0800

    add Header component to test sass

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,12 @@
+/*
+ * @Author: qianzhi
+ * @Date: 2022-04-27 22:52:08
+ * @LastEditors: qianzhi
+ * @LastEditTime: 2022-04-27 22:53:08
+ * @FilePath: /head-first-vite/src/components/Header/index.tsx
+ */
+import "./index.scss";
+
+export function Header() {
+  return <p className="header">This is Header</p>;
+}
