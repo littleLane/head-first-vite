9f5cd92eec1cbcfae29ef892cb3f4fd3cdbfcb14
littleLane <lzz857183384@163.com>
2022-04-27 22:26:59 +0800

    feat: 将 index.html 移入 src，并通过 vite.config 设置 root 为 src 目录

--- vite.config.ts
+++ vite.config.ts
@@ -2,16 +2,19 @@
  * @Author: qianzhi
  * @Date: 2022-04-22 23:35:58
  * @LastEditors: qianzhi
- * @LastEditTime: 2022-04-27 22:20:13
+ * @LastEditTime: 2022-04-27 22:24:53
  * @FilePath: /head-first-vite/vite.config.ts
  */
+import path from "path";
+
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  root: path.join(__dirname, "src"),
   plugins: [react()],
   server: {
-    port: 8888,
+    port: 3000,
   },
 });
