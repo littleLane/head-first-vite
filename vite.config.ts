a25027ed190c2720d31d9a6283a6a6ac3cc10ca4
littleLane <lzz857183384@163.com>
2022-04-27 23:02:33 +0800

    variable 样式文件支持自动引入

--- vite.config.ts
+++ vite.config.ts
@@ -2,17 +2,29 @@
  * @Author: qianzhi
  * @Date: 2022-04-22 23:35:58
  * @LastEditors: qianzhi
- * @LastEditTime: 2022-04-27 22:32:25
+ * @LastEditTime: 2022-04-27 23:01:48
  * @FilePath: /head-first-vite/vite.config.ts
  */
 import path from "path";
 
-import { defineConfig } from "vite";
+import { defineConfig, normalizePath } from "vite";
 import react from "@vitejs/plugin-react";
+
+// 全局的 scss 文件路径
+// 用 normalizePath 解决 window 下的路径问题
+const variablePath = normalizePath(path.resolve("./src/styles/variable.scss"));
 
 // https://vitejs.dev/config/
 export default defineConfig({
   root: path.join(__dirname, "src"),
+  css: {
+    preprocessorOptions: {
+      scss: {
+        // additionalData 的内容会在每个 scss 文件的开头自动注入
+        additionalData: `@import "${variablePath}";`,
+      },
+    },
+  },
   plugins: [react()],
   server: {
     port: 3000,
