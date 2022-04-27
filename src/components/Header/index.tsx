891ed0796f1c2ec408a91f9f13213bddfbb33f6f
littleLane <lzz857183384@163.com>
2022-04-27 23:04:46 +0800

    演示 cssModlue

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,11 +2,11 @@
  * @Author: qianzhi
  * @Date: 2022-04-27 22:52:08
  * @LastEditors: qianzhi
- * @LastEditTime: 2022-04-27 22:53:08
+ * @LastEditTime: 2022-04-27 23:04:01
  * @FilePath: /head-first-vite/src/components/Header/index.tsx
  */
-import "./index.scss";
+import styles from "./index.module.scss";
 
 export function Header() {
-  return <p className="header">This is Header</p>;
+  return <p className={styles.header}>This is Header</p>;
 }
