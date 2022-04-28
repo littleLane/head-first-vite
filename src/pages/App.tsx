7753d562fe88cdabbae86cde9cfad6eeab7e14f7
littleLane <lzz857183384@163.com>
2022-04-28 10:01:52 +0800

    演示 windi css attributify mode

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -2,13 +2,14 @@
  * @Author: qianzhi
  * @Date: 2022-04-22 23:35:58
  * @LastEditors: qianzhi
- * @LastEditTime: 2022-04-28 09:28:59
+ * @LastEditTime: 2022-04-28 09:54:59
  * @FilePath: /head-first-vite/src/pages/App.tsx
  */
 import { useState } from "react";
 import { Header } from "../components/Header";
 import { Footer } from "../components/Footer";
 import { WindiComponent } from "../components/WindiComponent";
+import { WindiAttributify } from "../components/WindiAttributify";
 import logo from "../assets/logo.svg";
 import "./App.css";
 
@@ -19,6 +20,7 @@ function App() {
     <div className="App">
       <Header />
       <WindiComponent />
+      <WindiAttributify />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>Hello Vite + React!</p>
---
+++ windi.config.ts
@@ -0,0 +1,13 @@
+/*
+ * @Author: qianzhi
+ * @Date: 2022-04-28 09:33:19
+ * @LastEditors: qianzhi
+ * @LastEditTime: 2022-04-28 09:33:19
+ * @FilePath: /head-first-vite/windi.config.ts
+ */
+import { defineConfig } from "vite-plugin-windicss";
+
+export default defineConfig({
+  // 开启 attributify
+  attributify: true,
+});
