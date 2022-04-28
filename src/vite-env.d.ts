4b7a6e487a0f8c186917533096f368e00da75ee7
littleLane <lzz857183384@163.com>
2022-04-28 10:01:17 +0800

    fix: windi css attributify mode 类型问题

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,14 @@
+/*
+ * @Author: qianzhi
+ * @Date: 2022-04-22 23:35:58
+ * @LastEditors: qianzhi
+ * @LastEditTime: 2022-04-28 09:52:34
+ * @FilePath: /head-first-vite/src/vite-env.d.ts
+ */
 /// <reference types="vite/client" />
+
+import { AttributifyAttributes } from "windicss/types/jsx";
+
+declare module "react" {
+  interface HTMLAttributes<T> extends AttributifyAttributes {}
+}
