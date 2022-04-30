2c0deb7359eb040476dc7a0d2225b40f9b31c2a2
littleLane <lzz857183384@163.com>
2022-04-30 09:35:20 +0800

    feat: 集成 commitlint

---
+++ .commitlintrc.js
@@ -0,0 +1,10 @@
+/*
+ * @Author: qianzhi
+ * @Date: 2022-04-30 09:33:42
+ * @LastEditors: qianzhi
+ * @LastEditTime: 2022-04-30 09:33:42
+ * @FilePath: /head-first-vite/.commitlintrc.js
+ */
+module.exports = {
+  extends: ['@commitlint/config-conventional']
+};
