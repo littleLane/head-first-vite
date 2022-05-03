53cfaa77cdd8913ba853e366f4cc2df551d005c7
littleLane <lzz857183384@163.com>
2022-05-03 12:52:28 +0800

    feat: 通过配置脚本演示 rollup

--- rollup/src/index.js
+++ rollup/src/index.js
@@ -2,8 +2,11 @@
  * @Author: qianzhi
  * @Date: 2022-05-03 12:43:56
  * @LastEditors: qianzhi
- * @LastEditTime: 2022-05-03 12:44:13
+ * @LastEditTime: 2022-05-03 12:50:05
  * @FilePath: /head-first-vite/rollup/src/index.js
  */
 import { add } from './util';
 console.log(add(1, 2));
+
+import { merge } from 'lodash';
+console.log(merge);
