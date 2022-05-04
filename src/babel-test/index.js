0137da73736b3e43cd24ca32ad120cb15a6450c0
littleLane <lzz857183384@163.com>
2022-05-04 15:18:18 +0800

    feat: babel 配置

---
+++ src/babel-test/index.js
@@ -0,0 +1,12 @@
+/*
+ * @Author: qianzhi
+ * @Date: 2022-05-04 15:09:45
+ * @LastEditors: qianzhi
+ * @LastEditTime: 2022-05-04 15:09:51
+ * @FilePath: /head-first-vite/src/babel-test/index.js
+ */
+const func = async () => {
+  console.log(12123);
+};
+
+Promise.resolve().finally();
